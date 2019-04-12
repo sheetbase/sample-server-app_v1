0fde19ef68c11472c1e04d576b3224488884667a
Lam Nhan <lamnhan92@gmail.com>
2019-04-13 01:22:50 +0700

    update rc0

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import { sheetbase } from '@sheetbase/core-server';
+import { sheetbase } from '@sheetbase/server';
 
+// create a server instance
 export const Sheetbase = sheetbase();
 
+// register a GET route to /
 Sheetbase.Router.get('/', (req, res) => {
     return res.send('Hello, world!');
 });
 
+// register a POSt route to /
 Sheetbase.Router.post('/', (req, res) => {
     return res.json({ hello: 'World!' });
 });
